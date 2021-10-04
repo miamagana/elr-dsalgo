689fcf28904ae789eb3b5c55c4df3629f353ae8a
miamagana <miguelangelmagana27@gmail.com>
2021-10-04 17:37:12 +0200

    Used theme in the core app, built the structure for starting with the 3 pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
-
-import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+import { AppComponent } from './core/components/main/app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
+import { CoreModule } from './core/core.module';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [
+    BrowserModule,
+    CoreModule,
+    BrowserAnimationsModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the app is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000',
+    }),
+    HttpClientModule,
+  ],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/core/core.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './components/header/header.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTabsModule } from '@angular/material/tabs';
+import { AppComponent } from './components/main/app.component';
+import { TranslocoRootModule } from '../transloco-root.module';
+import { TabsComponent } from './components/tabs/tabs.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { CourseComponent } from './components/course/course.component';
+
+@NgModule({
+  declarations: [
+    HeaderComponent,
+    AppComponent,
+    TabsComponent,
+    HomeComponent,
+    AboutComponent,
+    CourseComponent,
+  ],
+  imports: [CommonModule, TranslocoRootModule, MatToolbarModule, MatTabsModule],
+})
+export class CoreModule {}
