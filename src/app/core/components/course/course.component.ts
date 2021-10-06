3069df9aa58522b3909e69dc62d49d89c115586f
miamagana <miguelangelmagana27@gmail.com>
2021-10-06 13:49:13 +0200

    Made responsive stepper, tabs and added some content

--- src/app/core/components/course/course.component.ts
+++ src/app/core/components/course/course.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { StepperOrientation } from '@angular/material/stepper';
+import { BreakpointObserver } from '@angular/cdk/layout';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-course',
   templateUrl: './course.component.html',
-  styleUrls: ['./course.component.scss']
+  styleUrls: ['./course.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class CourseComponent implements OnInit {
+export class CourseComponent {
+  stepperOrientation: Observable<StepperOrientation>;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private readonly breakpointObserver: BreakpointObserver) {
+    this.stepperOrientation = this.breakpointObserver
+      .observe('(min-width: 800px)')
+      .pipe(map(({ matches }) => (matches ? 'horizontal' : 'vertical')));
   }
-
 }
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatTabsModule } from '@angular/material/tabs';
+import { MatStepperModule } from '@angular/material/stepper';
 import { AppComponent } from './components/main/app.component';
 import { TranslocoRootModule } from '../transloco-root.module';
 import { TabsComponent } from './components/tabs/tabs.component';
@@ -19,6 +20,12 @@ import { CourseComponent } from './components/course/course.component';
     AboutComponent,
     CourseComponent,
   ],
-  imports: [CommonModule, TranslocoRootModule, MatToolbarModule, MatTabsModule],
+  imports: [
+    CommonModule,
+    TranslocoRootModule,
+    MatToolbarModule,
+    MatTabsModule,
+    MatStepperModule,
+  ],
 })
 export class CoreModule {}
