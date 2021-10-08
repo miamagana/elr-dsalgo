69bf8469bad1fe81897fd647b581cf55a815a76c
miamagana <miguelangelmagana27@gmail.com>
2021-10-08 13:56:42 +0200

    Added firebase hosting

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,22 @@ import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
 import { CoreModule } from './core/core.module';
 import { HttpClientModule } from '@angular/common/http';
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
 
 @NgModule({
   imports: [
     BrowserModule,
+    provideFirebaseApp(() =>
+      initializeApp({
+        apiKey: 'AIzaSyCpU6fpulUHr-67posALuiyGW2_H03hSyU',
+        authDomain: 'dsalgo-elr.firebaseapp.com',
+        projectId: 'dsalgo-elr',
+        storageBucket: 'dsalgo-elr.appspot.com',
+        messagingSenderId: '600915565499',
+        appId: '1:600915565499:web:ca290d8217a31179de829e',
+        measurementId: 'G-9GT0H22NGW',
+      })
+    ),
     CoreModule,
     BrowserAnimationsModule,
     ServiceWorkerModule.register('ngsw-worker.js', {
